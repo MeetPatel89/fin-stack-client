80de106d9d6df237c4fe44feb0b2b41aa2ac8f97
Meet Patel <meetaiims@yahoo.com>
2021-01-10 21:47:18 -0600

    add rudimentary css to nav bar

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './Nav.css';
 
 export default class Nav extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-        }
-    }
-
-    render() {
-        return (
-          <nav className='navigation'>
-            <ul className='nav-links'>
-              <li>
-                <Link to='/'>Home</Link>
-              </li>
-              <li>Spending</li>
-              <li>Transactions</li>
-              <li>Stats</li>
-              <li>Accounts</li>
-              <li onClick={this.props.handleClick}>LogOut</li>
-            </ul>
-          </nav>
-        );
-    }
+  render() {
+    return (
+      <nav className='navigation'>
+        <ul className='nav-links'>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/transactions'>Transactions</Link>
+          </li>
+          <li><Link to='/categories'>Categories</Link></li>
+          <li><Link to='/stats'>Stats</Link></li>
+          <li><Link to='/accounts'>Accounts</Link></li>
+          <li id="log-out" onClick={this.props.handleClick}>LogOut</li>
+        </ul>
+      </nav>
+    );
+  }
 }
