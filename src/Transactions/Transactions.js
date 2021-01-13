0a7c39078b2f62c5dc6c215b3ee400c2a720631f
Meet Patel <meetaiims@yahoo.com>
2021-01-13 17:20:51 -0600

    add display transactions for selected date

--- src/Transactions/Transactions.js
+++ src/Transactions/Transactions.js
@@ -40,17 +40,32 @@ export default class Transactions extends Component {
   }
 
   render() {
+    const dates = this.state.transactions.map(transaction => new Date(transaction.date_time).toDateString());
+    console.log(dates);
+    const dailyTransactions = (this.state.transactions) &&
+        this.state.transactions.filter(transaction => new Date(transaction.date_time).toDateString() === this.state.selectedDate.toDateString())
+    const transactionsDisplay = (dailyTransactions.length) ?
+        dailyTransactions.map((transaction, i) => (
+            <div key = {i} className="display-transaction">
+            <span className="category">{transaction.category}</span>
+            <span className="amount">{transaction.amount}</span>
+            <span className="date">{new Date(transaction.date_time).toDateString()}</span>
+            <span className="account">{transaction.accounts}</span>
+
+        </div>
+        )) :
+        <div className="no-transactions">There are no transactions for this day</div>
+    
     return (
-      <>
+      <div>
         <div className='date-picker'>
           <DatePicker
             selected={this.state.selectedDate}
             onChange={(date) => this.handleChange(date)}
           />
         </div>
-
-        <p>Hello Transactions</p>
-      </>
+        {transactionsDisplay}
+      </div>
     );
   }
 }
